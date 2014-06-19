74e278abed5a508e14b97678b77ce57a669a9465
Ashley Nolan <anolan1@anolan1.local>
2014-06-19 17:42:54 +0100

    Changed all of our db setting to use promises rather than callbacks to try out ES6 promise functionality

--- app/controllers/setupController.js
+++ app/controllers/setupController.js
@@ -59,15 +59,16 @@ exports.createStates = function () {
 		//first get all the questions
 		Symbol.loadAll(function (err, symbols) {
 
-			var symbolsLength = symbols.length;
-
-			console.log('setupController: createStates: Number of Symbols: ' + symbolsLength);
+			var symbolsToCheck = [];
 
 			symbols.forEach(function (s) {
-				console.log('BOO');
-				_this.checkState(s, function (state) {
-					resolve();
-				});
+				symbolsToCheck.push(_this.checkState(s));
+			});
+
+			return Promise.all(
+				symbolsToCheck
+			).then(function () {
+				resolve();
 			});
 		});
 
@@ -76,45 +77,55 @@ exports.createStates = function () {
 
 exports.checkState = function (symbol) {
 
-	var hashtagLength = symbol.hashtags.length,
-		stateCheckCounter = 0;;
+	return new Promise(function (resolve, reject) {
 
-	_.each(symbol.hashtags, function (value, key) {
+		var hashtagsToCheck = [];
 
-		console.log('setupController: checkState: Checking state for ' + value.tagname);
+		_.each(symbol.hashtags, function (value, key) {
+			hashtagsToCheck.push(_this.checkHashtagState(value));
+		});
 
-		State.load(value._id, 'today', function (err, currentState) {
+		return Promise.all(
+			hashtagsToCheck
+		)
+		.then(function () {
+			resolve();
+		});
+	});
 
-			stateCheckCounter++;
+
+};
+
+exports.checkHashtagState = function (hashtag) {
+
+	return new Promise(function (resolve, reject) {
+
+		console.log('setupController: checkState: Checking state for ' + hashtag.tagname);
+
+		State.load(hashtag._id, 'today', function (err, currentState) {
 
 			//if we can find state, great
 			if (currentState) {
-				console.log('setupController: checkState: State found for ' +  value.tagname);
-				if (stateCheckCounter === hashtagLength) {
-					return currentState;
-				}
+				console.log('setupController: checkState: State found for ' +  hashtag.tagname);
+				resolve();
 
 			//else create a state in the DB and set to zero
 			} else {
-				console.log('setupController: checkState: State not found, so creating state myself', value.tagname);
+				console.log('setupController: checkState: State not found, so creating state myself', hashtag.tagname);
 
-				state.create(value, function (err) {
+				state.create(hashtag, function (err) {
 
 					if (err) {
 						console.log('setupController: checkState: ' + err + ': state not saved\n');
 					} else {
 						console.log('setupController: checkState: State saved to collection\n');
 					}
-
-					if (stateCheckCounter === hashtagLength) {
-						return state;
-					}
-
+					resolve();
 				});
 			}
 		});
+
 	});
 
-
-};
+}
 
